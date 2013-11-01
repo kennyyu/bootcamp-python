0372e647fd9b0bfbe0e04f2f8f10d20bb9efa845
Kenny Yu <yu.kenny124@gmail.com>
2013-11-01 00:11:27 -0400

    wrote some more exercises and unit tests

--- exercises/ex_strings.py
+++ exercises/ex_strings.py
@@ -1,3 +1,77 @@
 def num_words(s):
     words = s.split()
     return len(words)
+
+"""
+* number of words
+* most frequent n words
+* sum of the numbers in a list
+* return true if a appears in the list
+* words, text (return a set of all the words that are misspelled)
+* return number of unique words in string
+* return the longest word in sentence
+* sort the list and return in reverse order
+* return true if a string appears in list
+* return the array in reverse order
+* remove vowels
+
+tuples
+dictionaries
+
+* return most frequent word, and the frequency
+* return dictionary with word count
+"""
+
+def sum_list(lst):
+    return sum(lst)
+
+def appears_in_list(x, lst):
+    return x in lst
+
+def num_unique(lst):
+    return len(set(lst))
+
+def longest_word(lst):
+    sort_by_length = sorted(lst, key=len, reverse=True)
+    return sort_by_length[0]
+
+def reverse_list(lst):
+    return list(reversed(lst))
+
+def remove_vowels(s):
+    vowels = "aeiouAEIOU"
+    for vowel in vowels:
+        s = s.replace(vowel, "")
+    return s
+
+def word_frequency(lst):
+    freq = {}
+    for word in lst:
+        if word not in freq:
+            freq[word] = 0
+        freq[word] += 1
+    return freq
+
+def find_mismatch(lst1, lst2):
+    mismatch = []
+    for (word1, word2) in zip(lst1, lst2):
+        if word1 != word2:
+            mismatch.append((word1, word2))
+    return mismatch
+
+def most_frequent_word(lst):
+    freq = word_frequency(lst)
+    sort_by_freq = sorted(freq.items(), key=lambda (word,count): count, reverse=True)
+    return sort_by_freq[0]
+
+def spell_checker(vocab_list, word_list):
+    vocab_set = set(vocab_list)
+    misspelled = set()
+    for word in word_list:
+        if word not in vocab_list:
+            misspelled.add(word)
+    return misspelled
+
+def spell_checker2(vocab_list, word_list):
+    vocab_set = set(vocab_list)
+    return [word for word in word_list if word not in vocab_set]
--- exercises/ex_strings_test.py
+++ exercises/ex_strings_test.py
@@ -1,19 +1,49 @@
 import unittest
-import ex_strings
+from ex_strings import *
 
 class TestNumWords(unittest.TestCase):
 
     def test_empty(self):
         s = ""
-        self.assertEqual(0, ex_strings.num_words(s))
+        self.assertEqual(0, num_words(s))
 
     def test_one(self):
         s = "abc"
-        self.assertEqual(1, ex_strings.num_words(s))
+        self.assertEqual(1, num_words(s))
 
     def test_many(self):
         s = "a b c def gjijklj higj"
-        self.assertEqual(6, ex_strings.num_words(s))
+        self.assertEqual(6, num_words(s))
+
+class TestSumList(unittest.TestCase):
+
+    def test_empty(self):
+        self.assertEqual(0, sum_list([]))
+
+    def test_one(self):
+        self.assertEqual(-1, sum_list([-1]))
+
+    def test_many(self):
+        self.assertEqual(7, sum_list([-2, -3, 5, 8, 0, -1]))
+
+class TestAppears(unittest.TestCase):
+
+    def test_empty(self):
+        self.assertEqual(False, appears_in_list("foo", []))
+
+    def test_positive(self):
+        self.assertEqual(True, appears_in_list("f", ["a", "b", "f", "d"]))
+
+    def test_negative(self):
+        self.assertEqual(False, appears_in_list("f", ["a", "b", "c"]))
+
+class TestReverse(unittest.TestCase):
+
+    def test_empty(self):
+        self.assertEqual([], reverse_list([]))
+
+    def test_many(self):
+        self.assertEqual(["a", "b", "c"], reverse_list(["c", "b", "a"]))
 
 if __name__ == '__main__':
     unittest.main()
