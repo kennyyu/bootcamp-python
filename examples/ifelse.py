bef3df3488b48ea1f7f1020e3c92c729a7e77eb4
Kenny Yu <yu.kenny124@gmail.com>
2013-11-04 01:24:12 -0500

    added ifelse

---
+++ examples/ifelse.py
@@ -0,0 +1,32 @@
+# True and False must be capitalized
+# To make variables, just declare it and
+# assign it a value. The spaces around the "=" are optional.
+b = True
+c = False
+d = 4
+e = "apple"
+
+# Example of an if-else-if statement.
+# To check if two quantities are equal, use "=="
+if d == 3:
+    print "d is 3"
+elif d == 4:
+    print "d is 4"
+elif d == 5:
+    print "d is 5"
+else:
+    print "d is not 3 or 4 or 5"
+
+# Use "==" for strings as well!
+if e == "apple":
+    print e
+else:
+    print "NOT APPLE"
+
+# Use "and" and "or" to take the logical and/or of
+# two boolean values.
+if b and c:
+    print "b and c are both True"
+
+if b or c:
+    print "at least one of b or c is True"
