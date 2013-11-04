40a86b82ea231cf46d729e7a26d591fc3d9afdf8
Kenny Yu <yu.kenny124@gmail.com>
2013-11-04 01:10:36 -0500

    update instructions for hello

--- exercises-hello/hello.py
+++ exercises-hello/hello.py
@@ -3,8 +3,9 @@
 # This is your first python program!
 # print out "hello world" to the terminal.
 #
-# When you are done, run ./test.sh in the terminal.
-# The test should pass.
+# When you are done, run the program with: python hello.py
+#
+# Run ./test.sh to make sure your program matches our expected output.
 #
 # TODO: write your code below
-
+print "hello world"
